a0f9dfd010421e12801bf0f6d10a6be57149e13a
Anish Joshi <67471527+Anishh-Joshi@users.noreply.github.com>
2022-09-19 19:53:35 +0545

    logos

--- lib/widgets/custom_thread_card.dart
+++ lib/widgets/custom_thread_card.dart
@@ -12,21 +12,23 @@ class ThreadCard extends StatelessWidget {
           color: Theme.of(context).cardColor,
           borderRadius: BorderRadius.circular(10)),
       width: double.infinity,
-      height: MediaQuery.of(context).size.height / 10,
-      child: ListTile(
-        leading: const CircleAvatarCustom(
-          radius: 20,
-          url: "/media/default.jpg",
-        ),
-        title: Text(
-          "Help Me !!",
-          style: Theme.of(context).textTheme.headline4,
-        ),
-        trailing: const Text("14m ago"),
-        subtitle: Text(
-          "So this is the sotru of anish joshi hakuna matatatattatata.so this is the sotru of anish joshi hakuna matatatattatata.so this is the sotru of anish joshi hakuna matatatattatata.so this is the sotru of anish joshi hakuna matatatattatata.so this is the sotru of anish joshi hakuna matatatattatataso this is the sotru of anish joshi hakuna matatatattatata",
-          overflow: TextOverflow.ellipsis,
-          style: Theme.of(context).textTheme.headline6,
+      height: MediaQuery.of(context).size.height / 8,
+      child: Center(
+        child: ListTile(
+          leading: const CircleAvatarCustom(
+            radius: 20,
+            url: "/media/default.jpg",
+          ),
+          title: Text(
+            "Anish Joshi says, Help Me !!",
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          trailing: const Text("14m ago"),
+          subtitle: Text(
+            "So this is the sotru of anish joshi hakuna matatatattatata.so this is the sotru of anish joshi hakuna matatatattatata.so this is the sotru of anish joshi hakuna matatatattatata.so this is the sotru of anish joshi hakuna matatatattatata.so this is the sotru of anish joshi hakuna matatatattatataso this is the sotru of anish joshi hakuna matatatattatata",
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.headline6,
+          ),
         ),
       ),
     );
